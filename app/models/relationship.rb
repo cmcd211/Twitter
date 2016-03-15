20bb45afcc5a4ce6c2b43b1ab8c1e04517825acd
Courtney McDuff <mcduff.courtney@gmail.com>
2016-03-14 20:22:25 -0500

    Set up Following

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,2 +1,6 @@
 class Relationship < ActiveRecord::Base
+  # This is the user that initates the action
+  belongs_to :user
+  # This is the user being followed
+  belongs_to :friend, class_name: "User"
 end
